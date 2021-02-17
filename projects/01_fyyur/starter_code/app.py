cee90a6d9ee9109b118575ce216e4f27b39eb590
Eman Hassan <emanhassan3466@gmail.com>
2021-02-17 20:43:37 +0200

    Add migration for class Venue

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -40,6 +40,7 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+migrate =Migrate(app, db)
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
